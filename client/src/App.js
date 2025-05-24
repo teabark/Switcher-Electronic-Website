9ab5b3f48f31b5233d82861afd8c86c25cf8795e
Denzel Jones <denzeljones442@gmail.com>
2025-05-24 09:01:47 +0300

    Fix route App.js

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   async function isAuth() {
     try {
-      const response = await fetch("http://localhost:5000/auth/is-verify", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/is-verify`, {
         method: "GET",
         headers: { token: localStorage.token },
       });
