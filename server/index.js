6d9c418c3dd0c3d3b24cc5d345446e167cbe2ff1
Denzel Jones <denzeljones442@gmail.com>
2026-01-02 17:56:04 +0300

    Update cors issue1

--- server/index.js
+++ server/index.js
@@ -9,9 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://switcher-electronic-website-1.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "token"],
+  origin: "https://switcher-electronic-website-1.onrender.com", // your frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 
